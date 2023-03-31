c19a39f71c3952a70c0b5dfed7e0959f4c760d05
jasonPonda <jasonvinda@gmail.com>
2023-03-31 14:09:02 +0200

    all files

---
+++ script.js
@@ -0,0 +1,16 @@
+function talk() {
+    var know = {
+        "Hi":"Hello",
+        "How Are You ?":"Great !",
+        "Bye":"Have A Nice Day !",
+        "Hello":"Hi , Whats Up"
+    }
+
+    var user = document.getElementById('userBox').value;
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+    if(user in know){
+        document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    } else {
+        document.getElementById('chatLog').innerHTML = "I do not understand !";
+    }
+}
